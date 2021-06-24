6bc4c8ff27f5035cd5e6e0e9808c286ac7183f98
tdargent1 <tdargent1@gmail.com>
2021-06-24 16:17:18 +0200

    Add REACT

---
+++ client/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import List from "./Cart/List";
+
+function Body({ theme }) {
+  return (
+    <div>
+      <List theme={theme} />
+    </div>
+  );
+}
+
+export default Body;
