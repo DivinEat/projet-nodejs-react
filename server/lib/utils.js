a87900ca46e310c9aab2a2df459a48fb7fb96e71
tdargent1 <tdargent1@gmail.com>
2021-07-21 20:26:42 +0200

    Generate credentials

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,5 +1,17 @@
 exports.prettifyValidationErrors = (errors) =>
-  Object.keys(errors).reduce((acc, err) => {
-    acc[err] = errors[err].message;
-    return acc;
-  }, {});
+    Object.keys(errors).reduce((acc, err) => {
+        acc[err] = errors[err].message;
+        return acc;
+    }, {});
+
+exports.generateCredentials = (merchantId) => {
+    const randomstring = require('randomstring');
+
+    return {
+        clientId: randomstring.generate(),
+        clientSecret: randomstring.generate(),
+        merchantId: merchantId
+    }
+};
+
+
--- server/models/sequelize/Credential.js
+++ server/models/sequelize/Credential.js
@@ -1,14 +1,14 @@
-const { Model, DataTypes } = require("sequelize");
+const {Model, DataTypes} = require("sequelize");
 const Merchant = require("./Merchant");
 const conn = require("../../lib/sequelize");
 
-class Credential extends Model {}
+class Credential extends Model {
+}
 
 Credential.init(
     {
         clientId: DataTypes.STRING,
         clientSecret: DataTypes.STRING,
-        status: DataTypes.BOOLEAN,
     },
     {
         sequelize: conn,
@@ -16,7 +16,7 @@ Credential.init(
     }
 );
 
-Credential.belongsTo(Merchant, { as: "merchant" });
-Merchant.hasMany(Credential, { foreignKey: "merchantId" });
+Credential.belongsTo(Merchant, {as: "merchant"});
+Merchant.hasMany(Credential, {foreignKey: "merchantId"});
 
 module.exports = Credential;
